e39ec11d9207e5ba67aa5aef844be2def9ae8ca2
ciroBorrelli <c.borrelli@sky-media.it>
2016-01-05 18:41:43 +0100

    CIRO: better threatment of strings with \n \r

--- src/main/java/it/ciroppina/testPurposes/controller/TXTFileController.java
+++ src/main/java/it/ciroppina/testPurposes/controller/TXTFileController.java
@@ -15,9 +15,9 @@ import java.util.Map;
 import java.util.Properties;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.inject.Model;
 import javax.enterprise.inject.Produces;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -26,7 +26,8 @@ import javax.inject.Named;
 // EL name
 // Read more about the @Model stereotype in this FAQ:
 // http://www.cdi-spec.org/faq/#accordion6
-@Model
+//@Model
+@SessionScoped
 @Named("textController")
 public class TXTFileController {
 
@@ -36,7 +37,6 @@ public class TXTFileController {
     @Produces
     @Named
     private String text;
-
     public String getText() {
 		return text;
 	}
@@ -46,78 +46,80 @@ public class TXTFileController {
 
 	@Produces
     @Named
-    private String current;
-	
+    private static String current;
 	public String getCurrent() {
 		return current;
 	}
 	public void setCurrent(String current) {
-		this.current = current;
+		TXTFileController.current = current;
 	}
 	
 	@Produces
     @Named
-    private String[] offsets;
-
+    private static String[] offsets;
 	public String[] getOffsets() {
 		return offsets;
 	}
 	public void setOffsets(String[] offsets) {
-		this.offsets = offsets;
+		TXTFileController.offsets = offsets;
 	}
 
 	@Produces
     @Named
-    private Map<String, Group> numbers;
-
+    private static Map<String, Group> numbers;
 	public Map<String, Group> getNumbers() {
 		return numbers;
 	}
 	public void setNumbers(Map<String, Group> nums) {
-		this.numbers = nums;
+		TXTFileController.numbers = nums;
 	}
 	
 	@Produces
     @Named
-    private Map<String, Group> vehicles;
-
+    private static Map<String, Group> vehicles;
 	public Map<String, Group> getVehicles() {
 		return vehicles;
 	}
 	public void setVehicles(Map<String, Group> vs) {
-		this.vehicles = vs;
+		TXTFileController.vehicles = vs;
 	}
 	
 	@Produces
     @Named
-    private Map<String, Group> people;
-
+    private static Map<String, Group> people;
 	public Map<String, Group> getPeople() {
 		return people;
 	}
 	public void setPeople(Map<String, Group> people) {
-		this.people = people;
+		TXTFileController.people = people;
 	}
 	
 	@Produces
     @Named
-    private javax.servlet.http.Part file; // +getter+setter
+    private static javax.servlet.http.Part file;
 	public javax.servlet.http.Part getFile() {
 		return file;
 	}
 	public void setFile(javax.servlet.http.Part file) {
-		this.file = file;
+		TXTFileController.file = file;
 	}
 	
 	@PostConstruct
     public void init() {
 		setup();
     	this.text = TXTFileLoader.TEXT;
-		this.current ="";
-		this.people = new HashMap<String, Group>();
-		this.vehicles = new HashMap<String, Group>();
-		this.numbers = new HashMap<String, Group>();
+		if (current == null) TXTFileController.current ="";
+		if (people == null) TXTFileController.people = new HashMap<String, Group>();
+		if (vehicles == null) TXTFileController.vehicles = new HashMap<String, Group>();
+		if (numbers == null) TXTFileController.numbers = new HashMap<String, Group>();
     }
+	
+	public void reset() {
+		current = "";
+		numbers = new HashMap<String, Group>();
+		people = new HashMap<String, Group>();
+		vehicles = new HashMap<String, Group>();
+	}
 	
 	private static String CAPITALIZED_WORDS = null;
 	private static String PHONE_NUMBERS = null;
@@ -131,10 +133,8 @@ public class TXTFileController {
 		try {
 			regex.load(new FileInputStream(new File(regexFile)));
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		CAPITALIZED_WORDS = regex.getProperty("REGEX_03");
@@ -155,25 +155,22 @@ public class TXTFileController {
 				"UPLOAD: got something wrong") );
         	return;
         }
-    	init();
+    	reset();
+    	setup();
     	facesContext.addMessage("",
     	new FacesMessage(FacesMessage.SEVERITY_INFO,
 			"Success! UPLOAD: got a text file",
 			"UPLOAD: got a text file") );
     }
 
-	public void receive() throws Exception {
+	public void receive(String what) throws Exception {
 		people = null;
-		
-		if (current.trim().length() < 1) {
-			facesContext.addMessage(null,
-				new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					"Failure! MDM: nothing to search",
-					"MDM: nothing to search"));
-			return;
-		}
-		Object[] ooo = TXTFileLoader.search(current).toArray();
-		offsets = Arrays.copyOf(ooo, ooo.length, String[].class);
+		current = what;
+		if (what.contains("\n")) current = what.substring(0, what.indexOf("\n"));
+		if (what.contains("\r")) current = what.substring(0, what.indexOf("\r"));
+
+		Object[] ooo = TXTFileLoader.search(what).toArray();
+		setOffsets( Arrays.copyOf(ooo, ooo.length, String[].class) );
 
 		try {
 			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO,
@@ -189,51 +186,41 @@ public class TXTFileController {
 	
 	public void extractPeople(long hm) {
 		offsets = null;
-		//mock
-		//this.people.put("De Filippis Gilda", ""+180L);
-		//this.people.put("CARILLO Gianfranco", ""+79L);
+		reset();
 		if (this.text == null || this.text.length() < 10) return;
 		if (people == null || people.size() < 1) {
 			Long frequency = hm;
 			FindEntity finder = new FindEntity(CAPITALIZED_WORDS, this.text);
 			Map<String, Group> groups = finder.groupWithMinFrequencyOf(frequency);
 			Map<String, Group> people = finder.people(groups);
-			this.people = people;
+			TXTFileController.people = people;
 		}
-		return;
-//		Iterator<String> iterator = people.keySet().iterator();
-//		while (iterator.hasNext()) {
-//			String k = iterator.next();
-//			this.people.put(k, ""+people.get(k).getCount() );
-//		}
 	}
 
 	public void extractNumbers(long hm) {
 		offsets = null;
-
+		reset();
 		if (this.text == null || this.text.length() < 10) return;
 		if (numbers == null || numbers.size() < 1) {
 			Long frequency = hm;
 			FindEntity finder = new FindEntity(PHONE_NUMBERS, this.text);
 			Map<String, Group> groups = finder.groupWithMinFrequencyOf(frequency);
 			Map<String, Group> nums = finder.numbers(groups);
-			this.numbers = nums;
+			TXTFileController.numbers = nums;
 		}
-		return;
 	}
 	
 	public void extractVehicles(long hm) {
 		offsets = null;
-
+		reset();
 		if (this.text == null || this.text.length() < 10) return;
 		if (vehicles == null || vehicles.size() < 1) {
 			Long frequency = hm;
 			FindEntity finder = new FindEntity(VEHICLES, this.text);
 			Map<String, Group> groups = finder.groupWithMinFrequencyOf(frequency);
 			Map<String, Group> vs = finder.vehicles(groups);
-			this.vehicles = vs;
+			TXTFileController.vehicles = vs;
 		}
-		return;
 	}
 	
 	/**
